99c181eef0778a062a9ff43c5afe5612e860608d
“Corinne <corinne.canavan@gmail.com>
2022-09-12 20:54:04 -0600

    Set up folder structure and App files

---
+++ src/Components/App/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+const App = () => {
+
+  return (
+    <main className="main-container">
+      <header>
+        <h1>New York Times News Reader</h1>
+      </header>
+    </main>
+  )
+}
+
+export default App;
